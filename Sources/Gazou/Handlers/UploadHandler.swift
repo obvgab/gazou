84b867f4358c77a6ad9e22fbca5054d45ee98dea
Gab <gabriel@binarysky.ai>
2024-04-02 20:47:22 -0700

    Add OpenAPI endpoints for blob upload

--- Sources/Gazou/Handlers/UploadHandler.swift
+++ Sources/Gazou/Handlers/UploadHandler.swift
@@ -1,1 +1,13 @@
-extension UploadHandler {}
+extension UploadHandler {
+    func startUploadBlob(_ input: Operations.startUploadBlob.Input) async throws -> Operations.startUploadBlob.Output {
+        fatalError("Not done yet")
+    }
+    
+    func continueUploadBlob(_ input: Operations.continueUploadBlob.Input) async throws -> Operations.continueUploadBlob.Output {
+        fatalError("Not done yet")
+    }
+    
+    func finishUploadBlob(_ input: Operations.finishUploadBlob.Input) async throws -> Operations.finishUploadBlob.Output {
+        fatalError("Not done yet")
+    }
+}
